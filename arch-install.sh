80258c8547227068d36bd706488dbdc14e857d76
drardin <81466808+drardin@users.noreply.github.com>
2023-09-24 16:11:41 -0600

    Create arch-install.sh

---
+++ arch-install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# User-defined variables
+target_device="/dev/vda"         # Target device
+partition_size="+200G"           # Size of the primary partition
+rootvol_size="100%"              # Size of the root logical volume (100%FREE)
+
+# Create boot/EFI partition
+fdisk "$target_device" <<EOF
+n  # Create a new partition
+1  # Partition number 1
+   # Accept the default first sector
++512M  # Set the partition size to 512MB
+t  # Change partition type
+1  # Select partition 1
+1  # Set it as EFI System (EF00)
+w  # Write changes and exit
+EOF
+
+# Create primary partition for LVM
+fdisk "$target_device" <<EOF
+n  # Create a new partition
+2  # Partition number 2
+   # Accept the default first sector
+$partition_size  # Set the partition size
+t  # Change partition type
+2  # Select partition 2
+20  # Set it as Linux LVM (8e00)
+w  # Write changes and exit
+EOF
+
+# Display partition table info
+fdisk -l "$target_device"
+
+# Set up LVM
+pvcreate --dataalignment 1m "$target_device"2
+
+# Create a logical volume group
+vgcreate lvg0 "$target_device"2
+
+# Create logical volumes
+lvcreate -L "$rootvol_size" lvg0 -n rootvol
+lvcreate -l 100%FREE lvg0 -n homevol
+
+# Load the dm_mod kernel module
+modprobe dm_mod
+
+# Scan for volume groups
+vgscan
+
+# Activate volume group
+vgchange -ay
